929f7a3acde9d8c09f18cba816bd05a526c1faf7
Mori-orich <kelin0208@outlook.com>
2026-08-04 12:23:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,221 @@
+const app = document.getElementById("app");
 
+
+function startGame(){
+
+app.innerHTML = `
+
+<div class="card">
+
+<h1>
+💌 Birthday Adventure 💌
+</h1>
+
+
+<p>
+A special surprise was created only for my Duduzilla ❤️
+</p>
+
+
+<div class="heart" onclick="findHeart()">
+❤️
+</div>
+
+
+<p>
+Find the magic heart to start the adventure ✨
+</p>
+
+
+</div>
+
+`;
+
+}
+
+
+
+function findHeart(){
+
+app.innerHTML = `
+
+<div class="card">
+
+<h1>
+✨ You found it! ✨
+</h1>
+
+
+<p>
+The first secret is unlocked...
+</p>
+
+
+<button onclick="openEnvelope()">
+Open the surprise 💌
+</button>
+
+
+</div>
+
+`;
+
+}
+
+
+
+function openEnvelope(){
+
+app.innerHTML = `
+
+<div class="card">
+
+<h1>
+💌 A mysterious envelope
+</h1>
+
+
+<p>
+Something special is waiting inside...
+</p>
+
+
+<button onclick="showPassword()">
+Open the envelope
+</button>
+
+
+</div>
+
+`;
+
+}
+
+
+
+function showPassword(){
+
+app.innerHTML = `
+
+<div class="card">
+
+<h1>
+🔒 Secret Lock
+</h1>
+
+
+<p>
+Only Duduzilla knows the code ❤️
+</p>
+
+
+<input 
+id="code"
+placeholder="DD.MM"
+style="
+padding:15px;
+font-size:20px;
+border-radius:20px;
+border:2px solid pink;
+text-align:center;
+"
+>
+
+
+<br>
+
+
+<button onclick="checkCode()">
+Unlock ❤️
+</button>
+
+
+<p id="message"></p>
+
+
+</div>
+
+`;
+
+}
+
+
+
+function checkCode(){
+
+let code=document.getElementById("code").value;
+
+
+if(code=="09.08" || code=="0908"){
+
+app.innerHTML=`
+
+<div class="card">
+
+<h1>
+🔓 Unlocked!
+</h1>
+
+
+<p>
+You found the secret code ❤️
+</p>
+
+
+<button onclick="firstLetter()">
+Continue 💕
+</button>
+
+
+</div>
+
+`;
+
+}
+
+else{
+
+document.getElementById("message").innerHTML=
+"❌ Almost! Try again ❤️";
+
+}
+
+
+}
+
+
+
+function firstLetter(){
+
+app.innerHTML=`
+
+<div class="card">
+
+<h1>
+💖 Happy Birthday My Duduzilla 💖
+</h1>
+
+
+<p>
+
+Today is a very special day for a very special person.
+
+<br><br>
+
+Happy Birthday ❤️
+
+<br><br>
+
+More surprises are waiting...
+
+</p>
+
+
+</div>
+
+`;
+
+}
+
+
+startGame();
